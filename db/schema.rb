2c31d066a8c2559525c319e45bcf89833f8c63e3
doctorlam <chris.lam.unt@gmail.com>
2018-10-08 10:59:32 -0500

    devise gem runing correctly now. Had to rails generate devise controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_012520) do
+ActiveRecord::Schema.define(version: 2018_10_08_014320) do
 
   create_table "ingredients", force: :cascade do |t|
     t.text "content"
@@ -34,6 +34,18 @@ ActiveRecord::Schema.define(version: 2018_10_04_012520) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["recipe_id"], name: "index_steps_on_recipe_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
